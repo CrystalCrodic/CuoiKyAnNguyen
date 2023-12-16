710a8f49ae459ca10499f8272118e67d92dee282
Biện Phát <145833932+CrystalCrodic@users.noreply.github.com>
2023-12-16 09:51:23 +0700

    Update DBHelper.java

--- app/src/main/java/stu/edu/vn/onthicuoiky/db/DBHelper.java
+++ app/src/main/java/stu/edu/vn/onthicuoiky/db/DBHelper.java
@@ -11,44 +11,53 @@ import androidx.annotation.Nullable;
 import stu.edu.vn.onthicuoiky.models.SinhVien;
 
 public class DBHelper extends SQLiteOpenHelper {
+    // Lệnh tạo Bảng
     private final String TABLE = "CREATE TABLE IF NOT EXISTS SINHVIEN(masv INTEGER PRIMARY KEY AUTOINCREMENT, tensv VARCHAR(50), gioitinh VARCHAR(5))";
 
+    // Lệnh Xoá Bản
     private final String DROP = "DROP TABLE IF EXISTS SINHVIEN";
 
+    // Contructor (MainActivity.this, "tên database.sqlite", null, 1)
     public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, null, 1);
     }
 
+    // Gọi hàm tạo table ở đây
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TABLE);
     }
-
+    
+    // Gọi Hàm xoá Table ở đây
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(DROP);
     }
 
+    // Hàm Lấy data (Only dùng cho Select * From ...)
     public Cursor GetData(String sql){
         SQLiteDatabase db = getReadableDatabase();
         return db.rawQuery(sql, null);
     }
 
+    // Không Dụng Tới
     public void QueryData(String sql){
         SQLiteDatabase db = getWritableDatabase();
         db.execSQL(sql);
     }
 
+    // Làm Hàm Thêm Tuỳ Theo Table
     public void InsertData(SinhVien sv){
-        SQLiteDatabase db = getWritableDatabase();
-        String sql = "INSERT INTO SINHVIEN VALUES(null, ?, ?)";
-        SQLiteStatement pstm = db.compileStatement(sql);
-        pstm.clearBindings();
-        pstm.bindString(1, sv.getTenSV());
-        pstm.bindString(2, sv.getGioitinh());
-        pstm.executeInsert();
+        SQLiteDatabase db = getWritableDatabase(); // Cho Phép Sửa Table
+        String sql = "INSERT INTO SINHVIEN VALUES(null, ?, ?)"; // Giống PHP
+        SQLiteStatement pstm = db.compileStatement(sql); // Chạy lệnh SQL trên
+        pstm.clearBindings(); // Không cần ghi
+        pstm.bindString(1, sv.getTenSV()); // ? thứ nhất bind vào
+        pstm.bindString(2, sv.getGioitinh()); // ? thứ 2 bind vào
+        pstm.executeInsert(); // Thực Thi SQL
     }
 
+    // Như Insert
     public void DeleteData(SinhVien sv){
         SQLiteDatabase db = getWritableDatabase();
         String sql = "DELETE FROM SINHVIEN WHERE masv = ?";
